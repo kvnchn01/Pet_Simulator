c5bdca6edff32162ec08330351ca4bffd616efdf
Drippls <43197295+JudeLakkis@users.noreply.github.com>
2020-07-31 13:18:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ class Pet:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    
+    def show(self):
+        print(f"Pet: {self.name}\nAge: {self.age}")
 
 
 kentaro = Pet('kentaro', 4)
+kentaro.show()
