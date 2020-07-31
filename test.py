4c9a5c178015f748c5ed3742ab17bad7c4911c95
kvnchn01 <cleanhead111@gmail.com>
2020-07-31 13:19:28 +0800

    Kentaro has aged

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ class Pet:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-    
+
     def show(self):
         print(f"Pet: {self.name}\nAge: {self.age}")
 
 
-kentaro = Pet('kentaro', 4)
+kentaro = Pet('kentaro', 5)
 kentaro.show()
